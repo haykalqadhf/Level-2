0ac3838ed5fae961062a6fc9911c1d8980891275
haykalqadhf <164607679+haykalqadhf@users.noreply.github.com>
2024-10-29 11:34:09 +0700

    commit 2

---
+++ level2/Inventory.js
@@ -0,0 +1,38 @@
+/**
+ * TODO
+ * Selesaikan kode pembuatan class Inventory dengan ketentuan:
+ * - Memiliki properti `items` untuk menampung daftar item dalam bentuk array.
+ * - Memiliki method `addItem` untuk menambahkan item ke properti `items`.
+ * - Memiliki method `removeItem` untuk menghapus item berdasarkan `id`.
+ * - Memiliki method `listItems` untuk mengembalikan string yang merupakan informasi detail barang (dipanggil dari fungs `item.displayDetails()`).
+ */
+
+import Item from './Item.js';
+
+class Inventory {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    removeItem(id) {
+        this.items = this.items.filter(item => item.id !== id);
+    }
+
+    updateItem(id, newItem) {
+        const index = this.items.findIndex(item => item.id === id);
+        if (index !== -1) {
+            this.items[index] = newItem;
+        }
+    }
+
+    listItems() {
+        return this.items;
+    }
+}
+
+// Jangan hapus kode di bawah ini!
+export default Inventory;
